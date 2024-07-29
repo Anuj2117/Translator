48d8a85319c5d6e23caebc9252728d64e709e45a
Anuj2117 <anujkushwaha221@gmail.com>
2024-07-29 18:04:51 +0530

    add css

--- src/components/LanguageTranslator.jsx
+++ src/components/LanguageTranslator.jsx
@@ -2,74 +2,107 @@ import { useEffect, useState } from "react";
 import LanguageInput from "./LanguageInput";
 import TextInput from "./TextInput";
 import axios from "axios";
+import "../App.css";
 
 function LanguageTranslator() {
-    const [languages, setLanguages] = useState([]);
-    const [fromLanguage, setFromLanguage] = useState("");
-    const [toLanguage, setToLanguage] = useState("");
-    const [text, setText] = useState("");
-    const [translatedText, setTranslatedText] = useState("");
+  const [languages, setLanguages] = useState([]);
+  const [fromLanguage, setFromLanguage] = useState("");
+  const [toLanguage, setToLanguage] = useState("");
+  const [text, setText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
 
-    async function getLanguages() {
-        const url = 'https://text-translator2.p.rapidapi.com/getLanguages';
+  async function getLanguages() {
+    const url = "https://text-translator2.p.rapidapi.com/getLanguages";
 
-        const options = {
-            method: 'GET',
-            headers: {
-                'x-rapidapi-key': '4bd6365e42msh7ebf43a615ad500p1e410cjsn2673f641b4cd',
-                'x-rapidapi-host': 'text-translator2.p.rapidapi.com'
-            },
-        };
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "4bd6365e42msh7ebf43a615ad500p1e410cjsn2673f641b4cd",
+        "x-rapidapi-host": "text-translator2.p.rapidapi.com",
+      },
+    };
 
-        try {
-            const response = await axios(url, options);
-            const result = response.data;
-            setLanguages(result.data.languages);
-        } catch (error) {
-            console.error("Error fetching languages:", error);
-        }
+    try {
+      const response = await axios(url, options);
+      const result = response.data;
+      setLanguages(result.data.languages);
+    } catch (error) {
+      console.error("Error fetching languages:", error);
     }
+  }
 
-    async function translateText() {
-        const data = new FormData();
-        data.append('source_language', fromLanguage);
-        data.append('target_language', toLanguage);
-        data.append('text', text);
-        
-        const options = {
-          method: 'POST',
-          url: 'https://text-translator2.p.rapidapi.com/translate',
-          headers: {
-            'x-rapidapi-key': '4bd6365e42msh7ebf43a615ad500p1e410cjsn2673f641b4cd',
-            'x-rapidapi-host': 'text-translator2.p.rapidapi.com'
-          },
-          data: data
-        };
-        
-        try {
-            const response = await axios.request(options);
-            const translated = response.data.data.translatedText;
-            setTranslatedText(translated);
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
+  async function translateText() {
+    const data = new FormData();
+    data.append("source_language", fromLanguage);
+    data.append("target_language", toLanguage);
+    data.append("text", text);
+
+    const options = {
+      method: "POST",
+      url: "https://text-translator2.p.rapidapi.com/translate",
+      headers: {
+        "x-rapidapi-key": "4bd6365e42msh7ebf43a615ad500p1e410cjsn2673f641b4cd",
+        "x-rapidapi-host": "text-translator2.p.rapidapi.com",
+      },
+      data: data,
+    };
+
+    try {
+      const response = await axios.request(options);
+      const translated = response.data.data.translatedText;
+      setTranslatedText(translated);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        getLanguages();
-    }, []);
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
-    return (
-        <div>
-            <h1>Language Translator</h1>
-            <LanguageInput label="Translate from:" language={languages} selectedLanguage={fromLanguage} setSelectedLanguage={setFromLanguage} />
-            <LanguageInput label="Translate to:" language={languages} selectedLanguage={toLanguage} setSelectedLanguage={setToLanguage} />
-            <TextInput label="Enter Text:" value={text} onChange={(e) => setText(e.target.value)} />
-            <button onClick={translateText}>Translate</button>
-            <TextInput label="Translated Text:" value={translatedText} readOnly />
+  return (
+    <>
+      <div className="wrapper">
+        
+    
+        <h1>Language Translator</h1>
+        <div className="wrapper-2">
+        <div className="selectWrapper">
+          <LanguageInput
+            label="Translate from:"
+            language={languages}
+            selectedLanguage={fromLanguage}
+            setSelectedLanguage={setFromLanguage}
+          />
+          <LanguageInput
+            label="Translate to:"
+            language={languages}
+            selectedLanguage={toLanguage}
+            setSelectedLanguage={setToLanguage}
+          />
         </div>
-    );
+
+        <div className="wrapper3">
+
+        <div className="inputArea">
+          <TextInput
+            label="Enter Text:"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
+        </div>
+        <div className="output">
+        <TextInput label="Translated Text:" value={translatedText} readOnly />
+        </div>
+            </div>
+
+
+        <button onClick={translateText}>Translate</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LanguageTranslator;
